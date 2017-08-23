c35f065c4b594e9ca5319a21f7d75729593409b6
ingcarlosmoya <ingcarlosmoya@gmail.com>
2017-08-23 08:29:30 -0500

    The main project has been created. The product main views have been created and modified.

---
+++ Webshop/Models/ProductViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Webshop.Models
+{
+    public class ProductViewModel
+    {
+        public int Serial { get; set; }
+
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Price per unit")]
+        public decimal UnitPrice { get; set; }
+
+    }
+}
